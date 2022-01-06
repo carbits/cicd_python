1811a4fe192744b2f2d6824d6a010841297bb1d0
carti <cartigayane@gmail.com>
2022-01-06 16:11:59 -0500

    Update test_math_module.py

--- test_math_module.py
+++ test_math_module.py
@@ -4,7 +4,7 @@ def test_add():
     assert add(3, 2) == 5
 
 def test_subtract():
-    assert subtract(3, 2) == 1
+    assert subtract(3, 2) == 3
 
 def test_multiply():
     assert multiply(3, 2) == 6
